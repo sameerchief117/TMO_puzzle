d9c0ef8f9b4ecd8c485d92e3e4517de8288797ee
SKhan153 <Sameer.Khan153@T-Mobile.com>
2023-04-30 15:12:09 +0530

    Instant Search

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
@@ -35,6 +36,16 @@ export class BookSearchComponent implements OnInit {
     this.store.select(getAllBooks).subscribe(books => {
       this.books = books;
     });
+    this.searchForm.get('term').valueChanges
+    .pipe(debounceTime(500))
+    .subscribe(response=>{
+      if(response && response.length){
+        this.searchBooks()
+      }
+      else{
+        this.store.dispatch(clearSearch())
+      }
+    })
   }
 
   formatDate(date: void | string) {
