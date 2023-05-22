753f0799ee853128944f34dbc1c849a0e7a6f0f6
SKhan153 <Sameer.Khan153@T-Mobile.com>
2023-05-22 10:46:03 +0530

    test cases for instant search

--- apps/okreads-e2e/src/integration/search-books.spec.ts
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -6,13 +6,11 @@ describe('When: Use the search feature', () => {
   it('Then: I should be able to search books by title', () => {
     cy.get('input[type="search"]').type('javascript');
 
-    cy.get('form').submit();
-
     cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
   });
 
   it('Then: I should see search results as I am typing', () => {
-    cy.get('input[type="search"]').type('javascript');
+    cy.get('input[type="search"]').type('javascript', {delay: 500});
 
     cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
   });
--- libs/books/feature/src/lib/book-search/book-search.component.spec.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.spec.ts
@@ -1,9 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedTestingModule } from '@tmo/shared/testing';
 
 import { BooksFeatureModule } from '../books-feature.module';
 import { BookSearchComponent } from './book-search.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { clearSearch, getAllBooks, getBooksLoaded, searchBooks } from '@tmo/books/data-access';
+import { Book } from '@tmo/shared/models';
 
 describe('ProductsListComponent', () => {
   let component: BookSearchComponent;
@@ -24,4 +27,34 @@ describe('ProductsListComponent', () => {
   it('should create', () => {
     expect(component).toBeDefined();
   });
+
+  it('should search books with the search value', fakeAsync(() => {
+    component.searchForm.controls.term.setValue('testBook');
+    tick(500);
+    store.overrideSelector(getBooksLoaded, true);
+    store.overrideSelector(getAllBooks, [{ ...createBook('A'), isAdded: false }]);
+    store.refreshState();
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith(
+      searchBooks({ term: 'testBook' })
+    );
+  }));
+
+  it('should clear search if no search term is found', fakeAsync(() => {
+    component.searchForm.controls.term.setValue('');
+    tick(500);
+    store.refreshState();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      clearSearch()
+    );
+  }));
+
+  it('should search books with example', fakeAsync(() => {
+    fixture.detectChanges();
+    component.searchExample();
+    tick(500);
+    expect(store.dispatch).toHaveBeenCalledWith(
+      searchBooks({ term: 'javascript' })
+    );
+  }));
 });
